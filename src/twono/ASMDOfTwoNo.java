8af84d6433dead05dd2372d40ece79f2ec950f77
ankitnaitam <ankitnaitam2015@gmail.com>
2023-02-04 22:52:44 +0530

    changes by

---
+++ src/twono/ASMDOfTwoNo.java
@@ -0,0 +1,24 @@
+package twono;
+import java.util.*;
+public class ASMDOfTwoNo {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int sum= a+b;
+System.out.println("The sum of two no is:"+sum);
+int A=sc.nextInt();
+int B=sc.nextInt();
+int sub= A-B;
+System.out.println("The sub of two no is:"+sub);
+int c=sc.nextInt();
+int d=sc.nextInt();
+int mul= c*d;
+System.out.println("The mul of two no is:"+mul);
+int C=sc.nextInt();
+int D=sc.nextInt();
+int div= C/D;
+System.out.println("The div of two no is:"+div);
+
+}
+}
